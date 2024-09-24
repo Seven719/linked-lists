2b4675359d78b8b45d369de5447692ba0827d739
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-24 09:04:56 +0100

    added append method to LinkedList class
    set headNode and tailNode to null in constructor
    
    imported Node from ./node.js

---
+++ linked_list.js
@@ -0,0 +1,20 @@
+import Node from "./node.js";
+
+export default class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.headNode) {
+      this.headNode = newNode;
+      this.tailNode = newNode;
+    } else {
+      this.tailNode.nextNode = newNode;
+      this.tailNode = newNode;
+    }
+  }
+}
