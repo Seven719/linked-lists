ff9e07d4e2bb0c82f9b5820ca6ef876a410bd668
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-24 11:10:24 +0100

    added tests for methods using console logging

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
+list.prepend("dragon");
+
+// Should log the node at the head (dragon)
+console.log(list.head());
+
+// Should log the node at the tail (turtle)
+console.log(list.tail());
+
+// Should log the node at index 3 (parrot)
+console.log(list.at(3));
+
+// Should log
+console.log(`turtle is currently in the list: ${list.contains("turtle")}`);
+list.pop();
+console.log(`turtle is currently in the list: ${list.contains("turtle")}`);
+
+console.log(`cat is at index: ${list.find("cat")}\n`);
+
+list.insertAt("bison", 3);
+console.log("List after inserting bison: ");
+console.log(list.toString() + "\n");
+
+list.removeAt(3);
+console.log("List after remove node at index 3 (bison)");
+console.log(list.toString() + "\n");
+
+list.append("turtle");
+list.removeAt(0);
+console.log('List after removing node at 0 (dragon) and appending "turtle"');
+console.log(list.toString() + "\n");
