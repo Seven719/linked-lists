bc7203dbb604cf26ecf53c5381d1ad22be91bba2
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-24 09:13:43 +0100

    added size method
    added size variable to constructor
    
    added increase of size when appending or prepending to the list

--- linked_list.js
+++ linked_list.js
@@ -4,6 +4,7 @@ export default class LinkedList {
   constructor() {
     this.headNode = null;
     this.tailNode = null;
+    this.size = 0;
   }
 
   append(value) {
@@ -16,6 +17,8 @@ export default class LinkedList {
       this.tailNode.nextNode = newNode;
       this.tailNode = newNode;
     }
+
+    this.size++;
   }
 
   prepend(value) {
@@ -24,6 +27,12 @@ export default class LinkedList {
     if (!this.headNode) {
       this.tailNode = newNode;
     }
+
     this.headNode = newNode;
+    this.size++;
+  }
+
+  size() {
+    return this.size;
   }
 }
